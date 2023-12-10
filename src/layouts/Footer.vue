df3e8520083f94c6ee40802507199591f8dde2cb
zmzhang <8111354+zmzhang8@users.noreply.github.com>
2023-12-10 14:41:37 -0800

    Add GitHub link

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,23 +1,16 @@
 <template>
-  <v-footer color="secondary" height="68" :app=true>
-    <v-container class="fill-height">
-      <v-row justify="start">
-        <v-col cols="auto">
-          <span>Copyright © 2023 xyz.zeroblue</span>
-        </v-col>
-        <v-spacer></v-spacer>
-        <v-col cols="auto" class="py-1">
-          <v-btn variant="text" href="https://github.com/ZeroBlueXYZ/FileDirect/issues">
-            {{ $t("message.footer.support") }}
-          </v-btn>
-        </v-col>
-        <v-col cols="auto" class="py-1">
-          <v-btn variant="text" to="/privacy">
-            {{ $t("message.footer.privacy") }}
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
+  <v-footer color="secondary" height="40" :app=true>
+    <v-btn variant="text" href="https://github.com/ZeroBlueXYZ/FileDirect">
+      GitHub
+    </v-btn>
+    <v-btn variant="text" href="https://github.com/ZeroBlueXYZ/FileDirect/issues">
+      {{ $t("message.footer.support") }}
+    </v-btn>
+    <v-btn variant="text" to="/privacy">
+      {{ $t("message.footer.privacy") }}
+    </v-btn>
+    <v-spacer></v-spacer>
+    <span>Copyright © 2023 xyz.zeroblue</span>
   </v-footer>
 </template>
 
