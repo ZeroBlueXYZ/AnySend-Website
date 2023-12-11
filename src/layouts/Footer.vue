49a558ef6ead5b09bbe0961090daf2cc08a87bd9
zmzhang <8111354+zmzhang8@users.noreply.github.com>
2023-12-10 16:04:00 -0800

    Fix overflow in the footer

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,16 +1,23 @@
 <template>
-  <v-footer color="secondary" height="40" :app=true>
-    <v-btn variant="text" href="https://github.com/ZeroBlueXYZ/FileDirect">
-      GitHub
-    </v-btn>
-    <v-btn variant="text" href="https://github.com/ZeroBlueXYZ/FileDirect/issues">
-      {{ $t("message.footer.support") }}
-    </v-btn>
-    <v-btn variant="text" to="/privacy">
-      {{ $t("message.footer.privacy") }}
-    </v-btn>
-    <v-spacer></v-spacer>
-    <span>Copyright © 2023 xyz.zeroblue</span>
+  <v-footer color="secondary" :app=true>
+    <v-col>
+      <v-row justify="center">
+        <v-btn variant="text" href="https://github.com/ZeroBlueXYZ/FileDirect">
+          {{ $t("message.footer.github") }}
+        </v-btn>
+        <v-btn variant="text" href="https://github.com/ZeroBlueXYZ/FileDirect/issues">
+          {{ $t("message.footer.support") }}
+        </v-btn>
+        <v-btn variant="text" to="/privacy">
+          {{ $t("message.footer.privacy") }}
+        </v-btn>
+      </v-row>
+      <v-row justify="center" class="text-caption">
+        <span >
+          {{ $t("message.footer.copyright") }}
+        </span>
+      </v-row>
+    </v-col>
   </v-footer>
 </template>
 
